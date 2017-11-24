f72b87c6f02a0d791f6ee7ceeb32af3ecf73552d
Damian Peralta <peralta.damian@yahoo.com.mx>
2017-11-24 11:43:36 +0100

    Hasing passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id:10
-};
+var password = 'abc123';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt)=>{
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc')
-console.log('Decoded ----------------------');
-console.log(decoded);
+
+var hashedPassword = '$2a$10$GWRYxZP3ZhZPXjtM54mOPOz30VHt1b8.XF55wf4tJ0eLtMRkHniGa';
+
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+  console.log(res);
+});
+//
+// var data = {
+//   id:10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc')
+// console.log('Decoded ----------------------');
+// console.log(decoded);
 
 // var message = 'I am a patito';
 // var hash = SHA256(message).toString();
